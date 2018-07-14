c4b96a6dd7fa65e9295c0baf78f6205d4b617c35
isao_e <isao0214@gmail.com>
2018-07-14 22:15:41 +0900

    add samples/var

---
+++ samples/var.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // 整数型
+    int x;
+    x = 1;
+
+    // 実数型
+    float y = 1.0;
+
+    // 文字型（1文字）
+    char c = 'a';
+
+    printf("x = %d, y = %f, c = %c\n", x, y, c);
+
+    return 0;
+}
