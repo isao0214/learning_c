e609d180830eb32e676d46ba3073b0055bf3786f
isao_e <isao0214@gmail.com>
2018-07-14 22:15:23 +0900

    add samples/hello

---
+++ samples/hello.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+    ブロックコメント
+*/
+
+// 1行コメント
+
+int main(void) {
+    printf("hello world!\n");
+    return 0;
+}
+
+/*
+    # コンパイル方法
+
+    ```
+    $ gcc -o hello hello.c
+    ```
+
+    - hello.cのあるディレクトリで実行する
+    - hello というファイルが出力される
+*/
+
+/*
+    # 実行方法
+
+    ```
+    $ ./hello
+    ```
+*/
